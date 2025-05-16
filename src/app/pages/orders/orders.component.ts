0670564c7d4918702821bc4c0ac1241f40691699
FranzJairLTV <u202012378@upc.edu.pe>
2025-05-15 23:25:17 -0500

    feat: implement orders dashboard with expandable rows and status chips

---
+++ src/app/pages/orders/orders.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatBadgeModule } from '@angular/material/badge';
+
+@Component({
+  selector: 'app-orders',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatBadgeModule],
+  templateUrl: './orders.component.html',
+  styleUrl: './orders.component.css'
+})
+export class OrdersComponent {
+  displayedColumns: string[] = ['expand', 'created', 'user', 'amount', 'terminal', 'id', 'status'];
+  expandedElement: any | null = null;
+  toggleRow(row: any) {
+    this.expandedElement = this.expandedElement?.id === row.id ? null : row;
+  }
+  orders = [
+    { created: '4 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Callao', id: 'O_d1hfg...', status: 'Requested' },
+    { created: '4 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Pisco', id: 'O_d2hfg...', status: 'Approved' },
+    { created: '12 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Valero', id: 'O_d3hfg...', status: 'Released' },
+    { created: '12 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Valero', id: 'O_d4hfg...', status: 'Approved' },
+    { created: '18 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Pisco', id: 'O_d5hfg...', status: 'Requested' },
+    { created: '18 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Lurin', id: 'O_d6hfg...', status: 'Released' },
+    { created: '22 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Pisco', id: 'O_d7hfg...', status: 'Approved' },
+    { created: '23 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Lurin', id: 'O_d8hfg...', status: 'Closed' },
+    { created: '28 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Pisco', id: 'O_d9hfg...', status: 'Approved' },
+    { created: '28 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Lurin', id: 'O_d10hfg...', status: 'Closed' },
+    { created: '30 APR, 2025', user: 'Sebastian Rosas', amount: 'S/ 4000.00', terminal: 'Pisco', id: 'O_d11hfg...', status: 'Closed' },
+  ];
+}
